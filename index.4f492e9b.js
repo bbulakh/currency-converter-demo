896a5c61b17fccf383e6de80baae0c4298a9b270
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2023-12-27 18:17:43 +0300

    website updates

--- index.4f492e9b.js
+++ index.4f492e9b.js
@@ -180,9 +180,23 @@ function generateOptions(options) {
 // Add generated options to select inputs
 fromSelect.innerHTML = generateOptions(currencies);
 toSelect.innerHTML = generateOptions(currencies);
-fetch("https://openexchangerates.org/api/latest.json?app_id=fd990a81b594417cbff72b44d44316e6").then((response)=>response.json()).then((data)=>{
+fetch("https://openexchangerates.org/api/latest.json?app_id=fd990a81b594417cbff72b44d44316e6").then((response)=>{
+    if (!response.ok) throw new Error("Response Error");
+    return response.json();
+}).then((data)=>{
     window.exchangeRates = data.rates;
+    fromSelect.value = "USD";
+    toSelect.value = "RUB";
+}).catch((err)=>alert(err));
+// Add functionality for swap button
+document.querySelector("#swapButton").addEventListener("click", ()=>{
+    let inputFrom = document.querySelector("#fromCurrency");
+    let inputTo = document.querySelector("#toCurrency");
+    let inputSwapped = inputFrom.value;
+    inputFrom.value = inputTo.value;
+    inputTo.value = inputSwapped;
 });
+// Add functionality for convert button
 document.querySelector("#convertButton").addEventListener("click", ()=>{
     // Get the selected currencies and the amount to be converted
     const fromCurrency = fromSelect.value;
